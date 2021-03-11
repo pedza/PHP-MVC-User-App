64a13b6d4714e2b3638e2d1539166b8c8e92d235
Predrag Janevski <predrag-sk@hotmail.com>
2021-03-11 18:32:07 +0100

    master

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,7 +1,24 @@
 <?php
 
-    foreach ($data['users'] as $user) {
-        echo $user->name . "<br>";
-    }
+  require APPROOT . '/views/includes/head.php';
+
 
 ?>
+
+<div id="section-landing">
+
+    <?php
+      require APPROOT . '/views/includes/navigation.php';
+    ?>
+
+    <div class="wrapper-landing">
+        
+  
+        <h1>One man's crappy software</h1>
+        <h2>is another man's full time job</h2>
+
+
+    </div>
+
+</div>
+
